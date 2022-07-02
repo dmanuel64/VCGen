5bf371870ce79ffb17b88525409b40541d8d841d
Dylan Manuel <dylan.manuel@att.net>
2022-07-01 22:21:10 -0500

    basic command line

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(name = "Vulnerable Code Dataset Generator")]
+#[clap(author = "Dylan Manuel", version = "1.0")]
+#[clap(about = "Does awesome things", long_about = None)]
+pub struct CommandLineArgs {
+    /// Number of dataset entries
+    #[clap(parse(try_from_str = positive_value))]
+    entries: i32,
+    /// Ratio of vulnerable code entries to benign code entries
+    #[clap(short, long, parse(try_from_str = positive_percentage), default_value_t = 0.5)]
+    vulnerability_ratio: f32,
+}
+
+fn positive_value(arg: &str) -> Result<i32, String> {
+    let parsed_value: Result<i32, _> = arg.parse();
+    parsed_value
+        .or_else(|_| Err(format!("{arg} must be an integer.")))
+        .and_then(|value| {
+            if value > 0 {
+                Ok(value)
+            } else {
+                Err(format!("{value} must be a positive integer."))
+            }
+        })
+}
+
+fn positive_percentage(arg: &str) -> Result<f32, String> {
+    let parsed_value: Result<f32, _> = arg.parse();
+    parsed_value
+        .or_else(|_| Err(format!("{arg} must be a decimal.")))
+        .and_then(|value| {
+            if value > 0.0 && value <= 1.0 {
+                Ok(value)
+            } else {
+                Err(format!("{value} must be a percentage between 0% and 100%."))
+            }
+        })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use crate::config::CommandLineArgs;
+use clap::Parser;
+
+mod config;
+
 fn main() {
+    let args = CommandLineArgs::parse();
     println!("Hello, world!");
 }
